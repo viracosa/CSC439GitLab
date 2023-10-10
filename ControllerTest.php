ab9b9d0c2fb7582ba894f08ef7a9600c600c0e52
viracosa <78036745+viracosa@users.noreply.github.com>
2023-10-10 08:25:46 -0400

    Add files via upload

---
+++ ControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'Main.php';
+
+use PHPUnit\Framework\TestCase;
+
+class ControllerTest extends TestCase
+{
+    public function test_dummy() {
+        $this->assertTrue(true);
+    }
+}
+
+?>
